53e52fd7820bbf6a7fe332e174b748f6b143713f
AhmedHelalAhmed <ahmed.helal.online@gmail.com>
2018-06-01 21:51:04 +0200

    add bootstrap and front end validation

--- task_app/forms.py
+++ task_app/forms.py
@@ -1,7 +1,23 @@
 from django import forms
 from .models import Patient
 
+
 class PatientForm(forms.ModelForm):
     class Meta:
         model = Patient
+
+        labels = {
+            "patient_name": "Name",
+            "phone_number": "Phone"
+        }
+
         fields = ('patient_name', 'phone_number')
+
+        widgets = {
+            'patient_name': forms.TextInput(attrs={
+                'placeholder': 'Patient name'
+                , 'class': 'form-control'}),
+            'phone_number': forms.TextInput(attrs={
+                'placeholder': 'Patient phone number',
+                'class': 'form-control'}),
+        }
